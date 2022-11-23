a7ad201b1f0b520287e3b6a0e59670b09088bf48
Safat99 <sarkersafat99@gmail.com>
2022-11-23 16:39:33 +0600

    start project

---
+++ src/test/java/com/example/relationship_test/RelationshipTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relationship_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
